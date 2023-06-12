7a149ef1e2cdf689a3b550a6400e5cf863240f15
Leonardo Chinarelo <leonardo.chinarelo@hotmail.com>
2023-06-12 20:06:08 -0300

    prova finalizada

---
+++ ProdutosApi/Controllers/ProdutoController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProdutosApi.Models;
+using System.Collections.Immutable;
+
+namespace ProdutosApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProdutoController : ControllerBase
+{
+    private static List<Produto> products = new List<Produto>();
+
+    [HttpGet("/")]
+    public IActionResult ListProducts() => Ok(products.OrderBy(p => p.Id));
+
+    [HttpGet("/{id}")]
+    public IActionResult GetProduct(int id) => Ok(products.FirstOrDefault(p => p.Id == id));
+
+    [HttpPost("/")]
+    public IActionResult InsertProduct(Produto product)
+    {
+        products.Add(product);
+
+        return Ok(product);   
+    }
+
+    [HttpPut("/")]
+    public IActionResult UpdateProduct(Produto product)
+    {
+        var findProduct = products.FirstOrDefault(p => p.Id == product.Id);
+
+        products.Remove(findProduct);
+
+        products.Add(product);
+
+        return Ok(product);
+    }
+
+    [HttpDelete("/{id}")]
+    public IActionResult DeleteProduct(int id)
+    {
+        var product = products.FirstOrDefault(p => p.Id == id);
+
+        products.Remove(product);
+
+        return Ok("Produto removido com sucesso!");
+    }
+
+}
---
+++ ProdutosApi/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProdutosApi.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int Quantidade { get; set; }
+    public string Descricao { get; set; }
+    public string CodigoBarras { get; set; }
+}
